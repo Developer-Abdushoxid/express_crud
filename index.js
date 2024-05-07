8e3af6189d5a2bcfe9f61f2114f804686f6e8436
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-05-07 16:16:43 +0500

    add crud

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import userRoutes from './component/user.js';
 import express from 'express';
 import bodyParser from 'body-parser'
+import userRoutes from './component/users.js'
+
 const app = express();
 
 
-app.use('/user',userRoutes)
+app.use(bodyParser.json());
+
+app.use('/users', userRoutes);
+
+app.get('/', (req, res) =>{ res.send('HELLO FROM HOMEPAGE')})
+
+
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
